d11836353f58cacb0fba675cc3f65282b5d44daf
Hugo André Gomes Galvão <lahugo.galvao@Hugos-MacBook-Pro.local>
2020-01-22 16:36:29 -0300

    View list of all books with author and year

--- src/com/twu/biblioteca/view/WelcomeView.java
+++ src/com/twu/biblioteca/view/WelcomeView.java
@@ -1,12 +1,34 @@
 package com.twu.biblioteca.view;
 
+import com.twu.biblioteca.entity.Book;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class WelcomeView {
+
+    private List<Book> booksList = new ArrayList<Book>() {{
+        add(new Book("Harry Potter and the Sorcerer's stone", "J.K. Rowling", 2015));
+        add(new Book("The Last Wish: Introducing the Witcher", "Andrzej Sapkowski", 2008));
+        add(new Book("The Handmaid's tale", "Margaret Atwood", 1986));
+    }};
 
     public void run() {
         System.out.println(welcomeMessage());
+        System.out.println(listBooks());
     }
 
     public String welcomeMessage() {
         return "Welcome to Biblioteca. Your one-stop-shop for great books titles in Bangalore!";
+    }
+
+    public String listBooks() {
+        StringBuilder booksListStr = new StringBuilder();
+        for (Book book : booksList) {
+            booksListStr.append(book.toString());
+            booksListStr.append(System.getProperty("line.separator"));
+        }
+        return booksListStr.toString();
     }
 }
--- test/com/twu/biblioteca/view/WelcomeViewTest.java
+++ test/com/twu/biblioteca/view/WelcomeViewTest.java
@@ -14,4 +14,12 @@ public class WelcomeViewTest {
         String expected = "Welcome to Biblioteca. Your one-stop-shop for great books titles in Bangalore!";
         assertThat(welcomeView.welcomeMessage(), is(expected));
     }
+
+    @Test
+    public void listBooksTest() {
+        String expected = "Harry Potter and the Sorcerer's stone | J.K. Rowling | 2015\n" +
+                "The Last Wish: Introducing the Witcher | Andrzej Sapkowski | 2008\n" +
+                "The Handmaid's tale | Margaret Atwood | 1986\n";
+        assertThat(welcomeView.listBooks(), is(expected));
+    }
 }
